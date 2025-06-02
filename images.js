08a0b50dbc1db8e5092f70e1a35128e79ba0383c
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-02 15:52:40 -0700

    player movements
    player movements

---
+++ images.js
@@ -0,0 +1,5 @@
+export const worldmapImg = new Image();
+worldmapImg.src = "https://imgur.com/a/oU0BQMu";
+
+export const playerImg = new Image();
+playerImg.src = "https://imgur.com/a/MdZZyMT";
--- script.js
+++ script.js
@@ -5,9 +5,30 @@ const joinBtn = document.getElementById("joinBtn");
 const usernameInput = document.getElementById("usernameInput");
 const enterBtn = document.getElementById("enterBtn");
 const codeInput = document.getElementById("codeInput");
+const startBtn = document.getElementById("startBtn");
+
+import * as images from './images.js';
 
 let playerName = "";
 let roomCode = "-1";
+let gameOver = true;
+
+
+canvas.width = 1000;
+canvas.height = 600;
+
+let world_width = 2000;
+let world_height = 1200;
+
+let player = {
+    world_x: 250,
+    world_y: 600,
+    width: 100,
+    height: 100
+}
+
+let position_x = 0;
+let position_y = 0;
 
 createBtn.addEventListener("click", () => {
     const name = usernameInput.value.trim();
@@ -53,13 +74,40 @@ function enter_code_page() {
 }
 
 startBtn.addEventListener("click", () => {
-   update_game() 
-});
-
-function start_game() {
     document.getElementById("loadingPage").style.display = "none";
     document.getElementById("gameContainer").style.display = "block";
-    
+    gameOver = false;
+    gameLoop();
+});
 
-
+function background_map() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    position_x = Math.max(player.world_x, canvas.width/2);
+    position_y = Math.max(player.world_y, canvas.height/2);
+    position_x = Math.min(player.world_x, world_width - canvas.width/2);
+    position_y = Math.min(player.world_y, world_height - canvas.height/2);
+    ctx.drawImage(images.worldmapImg, position_x - canvas.width/2, position_y - canvas.height/2, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
 }
+
+function gameLoop() {
+    background_map();
+    ctx.drawImage(images.playerIMG, canvas.width/2 - player.width/2, canvas.height/2 - player.height/2, player.width, player.height);
+    requestAnimationFrame(gameLoop);
+}
+
+document.addEventListener("keydown", function(event) {
+    if (!gameOver) {
+        if (event.code === "KeyA") {
+            player.world_x -= 5;
+        }
+        if (event.code === "KeyD") {
+            player.world_x += 5;
+        }
+        if (event.code === "KeyW") {
+            player.world_y -= 5;
+        }
+        if (event.code === "KeyS") {
+            player.world_y += 5;
+        }
+    }
+})

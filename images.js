c3215cdc8bd90f6f54f9a8f451de0c9467f571ca
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-02 15:56:41 -0700

    small fix
    small fix

--- images.js
+++ images.js
@@ -1,5 +1,5 @@
 export const worldmapImg = new Image();
-worldmapImg.src = "https://imgur.com/a/oU0BQMu";
+worldmapImg.src = "https://imgur.com/a/oU0BQMu.png";
 
 export const playerImg = new Image();
-playerImg.src = "https://imgur.com/a/MdZZyMT";
+playerImg.src = "https://imgur.com/a/MdZZyMT.png";

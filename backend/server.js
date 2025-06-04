69c1ac21de5611d0adc74fd7cf260808808ca5d0
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-03 17:55:51 -0700

    cleaned up script.js
    cleaned up script.js

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,57 @@ const io = new Server(server);
 
 app.use(express.static("public")); 
 
-io.on("connection", (socket) => {
-    console.log(`Player connected: ${socket.id}`);
+const rooms = {}; 
 
-    socket.on("updatePlayer", (data) => {
-        socket.broadcast.emit("playerUpdate", data); // send to others
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("createRoom", ({ name }) => {
+        const roomCode = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+        socket.join(roomCode);
+        socket.roomCode = roomCode;
+
+        rooms[roomCode] = rooms[roomCode] || { players: {} };
+        rooms[roomCode].players[socket.id] = { name, x: 0, y: 0, angle: 0, weapon: 1 };
+
+        io.to(roomCode).emit("roomData", rooms[roomCode].players);
+        socket.emit("roomCreated", roomCode);
+    });
+
+    socket.on("joinRoom", ({ name, roomCode }) => {
+        if (!rooms[roomCode]) {
+            socket.emit("errorMessage", "Room code not found.");
+            return;
+        }
+        socket.join(roomCode);
+        socket.roomCode = roomCode;
+
+        rooms[roomCode].players[socket.id] = { name, x: 0, y: 0, angle: 0, weapon: 1 };
+
+        io.to(roomCode).emit("roomData", rooms[roomCode].players);
+    });
+
+    socket.on("move", (data) => {
+        const roomCode = socket.roomCode;
+        if (rooms[roomCode] && rooms[roomCode].players[socket.id]) {
+            Object.assign(rooms[roomCode].players[socket.id], data);
+            io.to(roomCode).emit("roomData", rooms[roomCode].players);
+        }
+    });
+
+    socket.on("shoot", (data) => {
+        const roomCode = socket.roomCode;
+        if (roomCode) {
+            socket.to(roomCode).emit("playerShot", { id: socket.id, ...data });
+        }
     });
 
     socket.on("disconnect", () => {
-        console.log(`Player disconnected: ${socket.id}`);
-        socket.broadcast.emit("playerLeft", socket.id);
+        const roomCode = socket.roomCode;
+        if (rooms[roomCode]) {
+            delete rooms[roomCode].players[socket.id];
+            io.to(roomCode).emit("roomData", rooms[roomCode].players);
+        }
     });
 });
 

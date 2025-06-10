5ab6f2448199baab608e698913e9d776ffe63f1e
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-10 09:47:55 -0700

    oops
    oops

--- docs/script/layout.js
+++ docs/script/layout.js
@@ -17,6 +17,6 @@ export function background_map() {
     constants.ctx.clearRect(0, 0, constants.ctx_width, constants.ctx_height);
     constants.position_x = Math.max(Math.min(constants.player.world_x, constants.world_width - constants.ctx_width/2), constants.ctx_width/2);
     constants.position_y = Math.max(Math.min(constants.player.world_y, constants.world_height - constants.ctx_height/2), constants.ctx_height/2);
-    constants.ctx.drawImage(images.originalworldmapImg, constants.position_x - constants.ctx_width/2, constants.position_y - constants.ctx_height/2, constants.ctx_width, constants.ctx_height, 0, 0, constants.ctx_width, constants.ctx_height);
+    constants.ctx.drawImage(WorldMap, constants.position_x - constants.ctx_width/2, constants.position_y - constants.ctx_height/2, constants.ctx_width, constants.ctx_height, 0, 0, constants.ctx_width, constants.ctx_height);
 }
 

251f8e2c82659465ba1ccaff53f388f1d937e8d9
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-09 21:38:45 -0700

    another quick fix
    another quick fix

--- docs/script/main.js
+++ docs/script/main.js
@@ -5,15 +5,15 @@ startUI.createBtn.addEventListener("click", () => {
     startUI.create_button();
 });
 
-joinBtn.addEventListener("click", () => {
+startUI.joinBtn.addEventListener("click", () => {
     startUI.join_button();
 });
 
-enterBtn.addEventListener("click", () => {
+startUI.enterBtn.addEventListener("click", () => {
     startUI.enter_button();
 });
 
-startBtn.addEventListener("click", () => {
+startUI.startBtn.addEventListener("click", () => {
     startUI.start_button();
     gameLoop.gameOver = false;
     gameLoop.create_worldmap();

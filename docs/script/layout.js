1a2852dcb97904ef728554ebd8beca12fb19e1d3
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-09 23:21:31 -0700

    double checking
    double checking

--- docs/script/layout.js
+++ docs/script/layout.js
@@ -18,5 +18,8 @@ export function background_map() {
     constants.position_x = Math.max(Math.min(constants.player.world_x, constants.world_width - constants.ctx_width/2), constants.ctx_width/2);
     constants.position_y = Math.max(Math.min(constants.player.world_y, constants.world_height - constants.ctx_height/2), constants.ctx_height/2);
     constants.ctx.drawImage(WorldMap, constants.position_x - constants.ctx_width/2, constants.position_y - constants.ctx_height/2, constants.ctx_width, constants.ctx_height, 0, 0, constants.ctx_width, constants.ctx_height);
+    constants.ctx.fillStyle = "black";
+    constants.ctx.fillRect(0, 0, constants.ctx_width, constants.ctx_height);
+
 }
 

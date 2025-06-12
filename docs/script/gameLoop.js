3c9ed4564f14109569207abdf5fb21abee6ff717
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-11 20:14:28 -0700

    removing frame
    removing frame

--- docs/script/gameLoop.js
+++ docs/script/gameLoop.js
@@ -2,19 +2,13 @@
 import * as players from './players.js';
 import * as layout from './layout.js';
 
-let frame = 0;
-
 export function game_loop() {
     //document.getElementById("arAmmo").textContent = weapons.assault_rife.ammo;
     //document.getElementById("sgAmmo").textContent = weapons.shotgun.ammo; 
 
-
     layout.background_map();
-    if (frame%6 === 0) {
-        players.move_player();
-    }
+    players.move_player();
     players.draw_players();
-    frame++;
 
     requestAnimationFrame(game_loop);
 }

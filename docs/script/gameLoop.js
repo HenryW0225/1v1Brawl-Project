6a4149ca6fbe3f15a4faf95a782ce774affd6c42
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-11 17:01:24 -0700

    fix
    fix

--- docs/script/gameLoop.js
+++ docs/script/gameLoop.js
@@ -1,25 +1,18 @@
-import * as weapons from './weapons.js';
+//import * as weapons from './weapons.js';
 import * as players from './players.js';
 import * as layout from './layout.js';
 
 let frame = 0;
 
 export function game_loop() {
-    document.getElementById("arAmmo").textContent = weapons.assault_rife.ammo;
-    document.getElementById("sgAmmo").textContent = weapons.shotgun.ammo; 
+    //document.getElementById("arAmmo").textContent = weapons.assault_rife.ammo;
+    //document.getElementById("sgAmmo").textContent = weapons.shotgun.ammo; 
 
 
     layout.background_map();
     if (frame%6 === 0) {
         players.move_player();
     }
-    /*if (constants.player.weapon === 1) {
-        weapons.fire_assault_rife();
-    } else {
-        weapons.fire_shotgun();
-    }
-    weapons.game_assault_rife();
-    weapons.game_shotgun();*/
     players.draw_players();
     frame++;
 

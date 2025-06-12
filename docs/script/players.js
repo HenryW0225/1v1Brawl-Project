bb7409d68addf9ffd534f330e69ca756c7595efb
HenryW0225 <140569708+HenryW0225@users.noreply.github.com>
2025-06-12 11:42:00 -0700

    fix player angle
    fix player angle

--- docs/script/players.js
+++ docs/script/players.js
@@ -24,11 +24,8 @@ export function move_player_locally() {
     session.player.world_x = Math.max(0, Math.min(constants.world_width, session.player.world_x + dx));
     session.player.world_y = Math.max(0, Math.min(constants.world_height, session.player.world_y + dy));
 
-    const camX = Math.max(Math.min(session.player.world_x, constants.world_width - constants.ctx_width / 2), constants.ctx_width / 2);
-    const camY = Math.max(Math.min(session.player.world_y, constants.world_height - constants.ctx_height / 2), constants.ctx_height / 2);
-
-    const worldMouseX = input.mouseX + camX - constants.ctx_width / 2;
-    const worldMouseY = input.mouseY + camY - constants.ctx_height / 2;
+    const worldMouseX = input.mouseX - constants.ctx_width / 2;
+    const worldMouseY = input.mouseY - constants.ctx_height / 2;
 
     session.player.angle = Math.atan2(worldMouseY - session.player.world_y, worldMouseX - session.player.world_x) + Math.PI / 2;
 }
